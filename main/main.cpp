53ed42668c6f1f02fbd269e549fab138bc9fbdd7
Corey <the-core92@hotmail.com>
2025-10-17 23:36:01 +1300

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -17,6 +17,8 @@
 #include "path_planning/rrt.hpp"
 #include "path_planning/rrt_star.hpp"
 
+using namespace path_planning;
+
 int main() {
   constexpr int n = 21;
   std::vector<std::vector<int>> grid(n, std::vector<int>(n, 0));
@@ -186,3 +188,4 @@ int main() {
 
   return 0;
 }
+
